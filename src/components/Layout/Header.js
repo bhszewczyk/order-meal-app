60cf5526f3cd2ae38101ff8e88b27789c075ae22
Barbara Szewczyk <comamysza@gmail.com>
2022-02-28 08:46:18 +0100

    Adds Header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import styles from './Header.module.css';
+import headerImg from '../../assets/meals.jpg';
+
+function Header(props) {
+	return (
+		<React.Fragment>
+			<header className={styles.header}>
+				<h1>Meals</h1>
+				<button>Cart</button>
+			</header>
+			<div className={styles['main-image']}>
+				<img src={headerImg} alt='A table full of delicious food.' />
+			</div>
+		</React.Fragment>
+	);
+}
+
+export default Header;
