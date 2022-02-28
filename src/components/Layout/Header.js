e28fe990ebbceb8424624f9d4ed841ea4af70be4
Barbara Szewczyk <comamysza@gmail.com>
2022-02-28 09:01:46 +0100

    Adds HeaderCartBtn

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,14 @@ import React from 'react';
 import styles from './Header.module.css';
 import headerImg from '../../assets/meals.jpg';
 
+import HeaderCartBtn from './HeaderCartBtn';
+
 function Header(props) {
 	return (
 		<React.Fragment>
 			<header className={styles.header}>
 				<h1>Meals</h1>
-				<button>Cart</button>
+				<HeaderCartBtn />
 			</header>
 			<div className={styles['main-image']}>
 				<img src={headerImg} alt='A table full of delicious food.' />
