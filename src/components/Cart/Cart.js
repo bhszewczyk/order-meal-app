437f415331e38bf9141cbc8c65fe7703c2f7fbe9
Barbara Szewczyk <comamysza@gmail.com>
2022-03-01 21:09:54 +0100

    Adds same cart items merging

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import styles from './Cart.module.css';
 import Modal from '../UI/Modal';
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 function Cart(props) {
+	const cartCtx = useContext(CartContext);
+
+	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+	const hasItems = cartCtx.items.length > 0;
+
+	const cartItemAddHandler = (item) => {};
+	const cartItemRemoveHandler = (id) => {};
+
 	const cartItems = (
 		<ul className={styles['cart-items']}>
-			{[{ id: 'c1', name: 'Hosomaki', amount: 2, price: 24.99 }].map((item) => (
-				<li key={item.id}>{item.name}</li>
+			{cartCtx.items.map((item) => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onAdd={cartItemAddHandler.bind(null, item)}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+				/>
 			))}
 		</ul>
 	);
@@ -15,15 +32,15 @@ function Cart(props) {
 	return (
 		<Modal onClick={props.onDisableCart}>
 			{cartItems}
-			<div className='styles.total'>
+			<div className={styles.total}>
 				<span>Total Amount</span>
-				<span>$$$</span>
+				<span>{totalAmount}</span>
 			</div>
 			<div className={styles.actions}>
 				<button className={styles['button--alt']} onClick={props.onDisableCart}>
 					Close
 				</button>
-				<button className={styles.button}>Order</button>
+				{hasItems && <button className={styles.button}>Order</button>}
 			</div>
 		</Modal>
 	);
