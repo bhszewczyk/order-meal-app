46204932b0f9c8e63280f4300fdc0fc3163e7940
Barbara Szewczyk <comamysza@gmail.com>
2022-03-26 19:39:13 +0100

    Moves dummy meals to firebase

--- src/components/Meals/MealsAvailable.js
+++ src/components/Meals/MealsAvailable.js
@@ -1,47 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from './MealsAvailable.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-	{
-		id: 'm1',
-		name: 'Sushi',
-		description: 'Finest fish and veggies',
-		price: 22.99,
-	},
-	{
-		id: 'm2',
-		name: 'Schnitzel',
-		description: 'A german specialty!',
-		price: 16.5,
-	},
-	{
-		id: 'm3',
-		name: 'Barbecue Burger',
-		description: 'American, raw, meaty',
-		price: 12.99,
-	},
-	{
-		id: 'm4',
-		name: 'Green Bowl',
-		description: 'Healthy...and green...',
-		price: 18.99,
-	},
-];
-
-const mealsList = DUMMY_MEALS.map((meal) => (
-	<MealItem
-		key={meal.id}
-		id={meal.id}
-		name={meal.name}
-		details={meal.description}
-		price={meal.price}
-	/>
-));
-
 function MealsAvailable() {
+	const [meals, setMeals] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [httpError, setHttpError] = useState(null);
+
+	useEffect(() => {
+		const fetchMeals = async () => {
+			const response = await fetch(
+				'https://meals-app-de3c1-default-rtdb.firebaseio.com/Meals.json'
+			);
+
+			if (!response.ok) {
+				throw new Error('Http request error');
+			}
+
+			const responseData = await response.json();
+
+			const loadedMeals = [];
+
+			for (const key in responseData) {
+				loadedMeals.push({
+					id: key,
+					name: responseData[key].name,
+					details: responseData[key].description,
+					price: responseData[key].price,
+				});
+			}
+
+			setMeals(loadedMeals);
+		};
+
+		fetchMeals()
+			.then(() => {
+				setIsLoading(false);
+			})
+			.catch((error) => {
+				console.log(error);
+				setIsLoading(false);
+				setHttpError(error.message);
+			});
+	}, []);
+
+	const mealsList = meals.map((meal) => (
+		<MealItem
+			key={meal.id}
+			id={meal.id}
+			name={meal.name}
+			details={meal.description}
+			price={meal.price}
+		/>
+	));
+
+	if (isLoading) {
+		console.log('isLoading');
+		return (
+			<section className={styles['meals-loading']}>
+				<p>Loading...</p>
+			</section>
+		);
+	}
+
+	if (httpError) {
+		return (
+			<section className={styles['meals-error']}>
+				<p>{httpError}</p>
+			</section>
+		);
+	}
+
 	return (
 		<section className={styles.meals}>
 			<Card>
